8e8f3b391bd7277eb64c6c6008f292b060492e05
mabrune <mabrune.inf@gmail.com>
2021-04-21 17:42:53 -0300

    OrderItem, many-to-many association with extra attributes - PART 2

--- src/main/java/com/mydomain/springbootjpa/config/TestConfig.java
+++ src/main/java/com/mydomain/springbootjpa/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.mydomain.springbootjpa.entities.Category;
 import com.mydomain.springbootjpa.entities.Order;
+import com.mydomain.springbootjpa.entities.OrderItem;
 import com.mydomain.springbootjpa.entities.Product;
 import com.mydomain.springbootjpa.entities.User;
 import com.mydomain.springbootjpa.entities.enums.OrderStatus;
 import com.mydomain.springbootjpa.repositories.CategoryRepository;
+import com.mydomain.springbootjpa.repositories.OrderItemRepository;
 import com.mydomain.springbootjpa.repositories.OrderRepository;
 import com.mydomain.springbootjpa.repositories.ProductRepository;
 import com.mydomain.springbootjpa.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,5 +73,12 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
--- src/main/java/com/mydomain/springbootjpa/entities/Order.java
+++ src/main/java/com/mydomain/springbootjpa/entities/Order.java
@@ -2,6 +2,8 @@ package com.mydomain.springbootjpa.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,30 +11,34 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mydomain.springbootjpa.entities.enums.OrderStatus;
 
 @Entity
-@Table(name= "tb_order")
+@Table(name = "tb_order")
 public class Order implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	private Integer orderStatus;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+
 	public Order() {
 	}
 
@@ -76,6 +82,10 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
 	}
 
 	@Override
